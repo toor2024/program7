4cf68809f169c2ea8a8379935817accff37c086f
toor2024 <atoor143143@gmail.com>
2024-11-13 23:49:41 -0600

    Media.h

--- CS201R-PGM6/Media.h
+++ CS201R-PGM6/Media.h
@@ -1,42 +1,42 @@
-#pragma once
-#include <string>
-#include <vector>
+#ifndef MEDIA_H
+#define MEDIA_H
+
 #include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
 
 class Media {
 protected:
-    char type;
-    std::string title;
-    std::string keyName;
+    char type;          // Movie, Book, or Song
+    string title;
+    string keyName;
     int rating;
-    std::string genre;
+    string genre;
     int length;
     int yearReleased;
 
 public:
-    Media();
-    Media(char type, std::string title, std::string keyName, int rating, std::string genre, int length, int yearReleased);
+    // Constructor
+    Media(char t, string tit, string kn, int r, string g, int l, int y) 
+        : type(t), title(tit), keyName(kn), rating(r), genre(g), length(l), yearReleased(y) {}
+
+    // Pure virtual function to make Media an abstract class
+    virtual void print(ostream& os) const = 0;
+
+    // Getters and setters
+    char getType() const { return type; }
+    string getTitle() const { return title; }
+    string getKeyName() const { return keyName; }
+    int getRating() const { return rating; }
+    string getGenre() const { return genre; }
+    int getLength() const { return length; }
+    int getYearReleased() const { return yearReleased; }
 
     // Setters
-    void setType(char c);
-    void setRating(int r);
-    void setYearReleased(int yr);
-
-    // Getters
-    char getType() const;
-    std::string getTitle() const;
-    std::string getKeyName() const;
-    int getRating() const;
-    std::string getGenre() const;
-    int getLength() const;
-    int getYearReleased() const;
-
-    // Pure virtual function for printing (abstract class)
-    virtual void print(std::ostream& out) const = 0;
-
-    // Static function to create a Media object from a line
-    static Media* createFromLine(const std::string& line, std::ostream& outErr);
-
-    // Virtual destructor for proper cleanup
-    virtual ~Media() = default;
+    void setRating(int r) { rating = r; }
+    void setYearReleased(int year) { yearReleased = year; }
 };
+
+#endif // MEDIA_H
