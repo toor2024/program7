20008adae88cc755455e879f1fad5dfe7425f146
toor2024 <atoor143143@gmail.com>
2024-11-13 23:49:56 -0600

    Movie.cpp

--- CS201R-PGM6/Movie.cpp
+++ CS201R-PGM6/Movie.cpp
@@ -1,17 +1,31 @@
 #include "Movie.h"
 #include <sstream>
 
-Movie::Movie(char type, std::string title, std::string keyName, int rating, std::string genre, int length, int yearReleased, std::vector<std::string> stars)
-    : Media(type, title, keyName, rating, genre, length, yearReleased), stars(stars) {}
+Movie* Movie::createFromLine(const std::string& line) {
+    std::stringstream ss(line);
+    std::string title, keyName, genre;
+    int rating, length, year;
+    std::vector<std::string> stars;
 
-std::vector<std::string> Movie::getStars() const {
-    return stars;
-}
+    // Parse the fields from the line
+    getline(ss, title, ',');
+    getline(ss, keyName, ',');
+    ss >> rating;
+    ss.ignore();  // Skip comma
+    getline(ss, genre, ',');
+    ss >> length;
+    ss.ignore(); // Skip comma
+    ss >> year;
 
-void Movie::setStars(const std::vector<std::string>& newStars) {
-    stars = newStars;
+    // Create and return Movie object
+    stars.push_back("Actor1");  // Example stars
+    stars.push_back("Actor2");
+    return new Movie('M', title, keyName, rating, genre, length, year, stars);
 }
 
 void Movie::print(std::ostream& out) const {
     out << "Movie: " << title << " by " << keyName << " (" << yearReleased << ") - Rating: " << rating << "/10\n";
+    for (const auto& star : stars) {
+        out << "Starring: " << star << "\n";
+    }
 }
