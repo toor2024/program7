a7c1cf97e55b21620f8887c21d85897aa5e9e8db
toor2024 <atoor143143@gmail.com>
2024-11-13 23:50:08 -0600

    Movie.h

--- CS201R-PGM6/Movie.h
+++ CS201R-PGM6/Movie.h
@@ -2,35 +2,28 @@
 #define MOVIE_H
 
 #include "Media.h"
-#include <vector>
 #include <string>
-
-using namespace std;
+#include <vector>
+#include <iostream>
 
 class Movie : public Media {
 private:
-    vector<string> stars;
+    std::vector<std::string> stars;
 
 public:
     // Constructor
-    Movie(string tit, string kn, int r, string g, int l, int y, vector<string> s)
-        : Media('M', tit, kn, r, g, l, y), stars(s) {}
+    Movie(char type, std::string title, std::string keyName, int rating, std::string genre, int length, int yearReleased, std::vector<std::string> stars)
+        : Media(type, title, keyName, rating, genre, length, yearReleased), stars(stars) {}
 
-    // Getter and Setter for stars
-    vector<string> getStars() const { return stars; }
-    void setStars(const vector<string>& newStars) { stars = newStars; }
+    // Getter and setter for stars
+    std::vector<std::string> getStars() const;
+    void setStars(const std::vector<std::string>& newStars);
 
-    // Implement the print function for Movie
-    void print(ostream& os, vector<Media*>& mediaList) override {
-        os << "Movie: " << title << "\n";
-        os << "Genre: " << genre << "\n";
-        os << "Rating: " << rating << "\n";
-        os << "Stars: ";
-        for (const auto& star : stars) {
-            os << star << " ";
-        }
-        os << "\n";
-    }
+    // Print function
+    void print(std::ostream& out) const override;
+
+    // Static function to create Movie from line
+    static Movie* createFromLine(const std::string& line);
 };
 
 #endif // MOVIE_H
